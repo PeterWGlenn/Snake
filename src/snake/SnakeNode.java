23e973fba7391668bbe76dd3ba1d47cbb0c7c494
Peter Glenn <peter.glenn.17@cnu.edu>
2019-02-25 14:40:33 -0500

    Added location and next private fields to SnakeNode and added getters and setters

--- src/snake/SnakeNode.java
+++ src/snake/SnakeNode.java
@@ -1,9 +1,48 @@
 package snake;
 
+import java.awt.Point;
+
+/**
+ * A data type to represent the body of the snake.
+ * 
+ * @author Peter Glenn
+ * @version 2.25.19
+ */
 public class SnakeNode {
 
-    public SnakeNode() {
+    private Point location;
+    private SnakeNode next;
 
+    /**
+     * The constructor for SnakeNode that stores a location and next value for a
+     * Snake Node
+     * 
+     * @param l
+     *            The location
+     * @param n
+     *            The next SnakeNode
+     */
+    public SnakeNode(Point l, SnakeNode n) {
+        location = l;
+        next = n;
+    }
+
+    /**
+     * Returns the location of the Snake Node
+     * 
+     * @return Point
+     */
+    protected Point getLocation() {
+        return location;
+    }
+
+    /**
+     * A getter that returns the next snake node
+     * 
+     * @return SnakeNode
+     */
+    protected SnakeNode next() {
+        return next;
     }
 
 }
