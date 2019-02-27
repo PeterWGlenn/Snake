efe85107ad308ba82c8354fc5b21528d8b7761d4
Peter Glenn <peter.glenn.17@cnu.edu>
2019-02-27 17:50:46 -0500

    Set up the basics of the Game class, including the main game loop

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,5 +1,89 @@
 package main;
 
-public class Game {
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements Runnable {
+
+    private static final long serialVersionUID = 1L;
+
+    private static boolean isRunning;
+
+    public static final double SCALE = 0.8;
+    public static final int WIDTH = (int) (2500 * SCALE);
+    public static final int HEIGHT = (int) (1500 * SCALE);
+    public static final int FPS = 60;
+
+    private static Game gameComponent = new Game();
+    private static JFrame frame = new JFrame();
+
+    public Game() {
+        setFocusable(true);
+        requestFocus();
+        start();
+    }
+
+    public void start() {
+        isRunning = true;
+        new Thread(this).start();
+    }
+
+    public void stop() {
+        isRunning = false;
+    }
+
+    @Override
+    public void run() {
+
+        while (isRunning) {
+
+            try {
+
+                // do things
+
+                double tick = 1000.0 / FPS;
+                Thread.sleep((long) tick);
+
+            }
+            catch (Exception e) {
+                System.out.println("Game loop exception: " + e.toString());
+            }
+        }
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Paint World
+        // world.render(g);
+
+        // Paint Snake
+
+        // Paint Apples
+
+        // Paint Text Area
+    }
+
+    public static void main(String[] args) {
+        Dimension size = new Dimension(WIDTH, HEIGHT);
+        frame.setTitle("Snake");
+        frame.setVisible(true);
+        frame.setSize(size);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(gameComponent);
+
+        // Keyboard Input
+        // frame.addKeyListener(new KeyboardInput());
+
+        // Spawn Snake and Apple
+        // Blob.reset();
+    }
 
 }
